8fcfb2cb7933072bad97d3d75bd7d1f50c21a359
lemonilemon <jaspertsai1019@gmail.com>
2023-04-05 14:32:14 +0800

    Optimize

--- main.py
+++ main.py
@@ -1,105 +1,30 @@
-import matplotlib.pyplot as plt
-import time
-import os
-import numpy as np
-from collections import OrderedDict
-import math
 import torch
 from torch import nn
-import torchvision.datasets
-from torchvision import transforms
-from torch.utils.data import Dataset 
-from torch.utils.data import DataLoader
-from torch.cuda.amp import autocast
-from torch.cuda.amp import GradScaler
+import torchvision
+from torchvision import datasets, transforms
+import torch.utils.data
+from torchvision.models import ResNet50_Weights, resnet50
+from torch.utils.tensorboard.writer import SummaryWriter
+from tensorboard import program
+from torch.cuda.amp.autocast_mode import autocast
+from torch.cuda.amp.grad_scaler import GradScaler
+from tqdm import tqdm
 
-if not os.path.exists(os.path.dirname("./checkpoints")):
-    os.makedirs("./checkpoints")
- 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device : {device}")
+# Constants
+EPOCHS = 30 
+BATCH_SIZE = 32
+NUM_WORKER = 8
+IMAGE_SIZE = 224
+LOG_DIR = "./runs"
 
-class Resblock(nn.Module):
-    def __init__(self, in_channels, out_channels, downsample = False):
-        super().__init__()
-        self.downsample = downsample
-        self.normalpath = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels // 4, kernel_size = 1, stride = 1),
-            nn.BatchNorm2d(out_channels // 4),
-            nn.ReLU(),
-            nn.Conv2d(out_channels // 4, out_channels // 4, kernel_size = 3, stride = 2 if downsample else 1, padding = 1),
-            nn.BatchNorm2d(out_channels // 4),
-            nn.ReLU(),
-            nn.Conv2d(out_channels // 4, out_channels, kernel_size = 1, stride = 1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
-        self.shortcut = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = 2 if downsample else 1),
-            nn.BatchNorm2d(out_channels)
-        )
-    def forward(self, x):
-       return self.normalpath(x) + self.shortcut(x)
+# Tensorboard
+writer = SummaryWriter(LOG_DIR)
+tb = program.TensorBoard()
+tb.configure(argv=[None, '--logdir', LOG_DIR])
+url = tb.launch()
+print(f"Tensorflow listening on {url}")
 
-class ResNet50(nn.Module):
-  def __init__(self, resblock, in_channels, out_features):
-    super().__init__() 
-    self.conv1 = nn.Sequential(
-        nn.Conv2d(in_channels, 64, kernel_size = 7, stride = 2, padding = 3),
-        nn.BatchNorm2d(64),
-        nn.ReLU()
-    )
-
-    self.conv2 = nn.Sequential(
-        nn.MaxPool2d(kernel_size = 3, stride = 2), 
-    )  
-    self.conv2.add_module("conv2_1", resblock(64, 256))
-    for i in range(1, 3):
-        self.conv2.add_module(f"conv2_{i + 1}", resblock(256, 256))
-
-    self.conv3 = nn.Sequential()
-    self.conv3.add_module("conv3_1", resblock(256, 512, downsample = True))
-    for i in range(1, 4):
-        self.conv3.add_module(f"conv3_{i + 1}", resblock(512, 512))
-
-    self.conv4 = nn.Sequential()
-    self.conv4.add_module("conv4_1", resblock(512, 1024, downsample = True))
-    for i in range(1, 6):
-        self.conv4.add_module(f"conv4_{i + 1}", resblock(1024, 1024))
-
-    self.conv5 = nn.Sequential()
-    self.conv5.add_module("conv5_1", resblock(1024, 2048, downsample = True))
-    for i in range(1, 3):
-        self.conv5.add_module(f"conv5_{i + 1}", resblock(2048, 2048))
-
-    self.gap = torch.nn.AdaptiveAvgPool2d(1)
-    self.fc = torch.nn.Linear(2048, out_features)
-
-  @autocast()
-  def forward(self, x):
-    out = self.conv1(x)
-    out = self.conv2(out)
-    out = self.conv3(out)
-    out = self.conv4(out)
-    out = self.conv5(out)
-    out = self.gap(out)
-    out = torch.flatten(out, 1)
-    out = self.fc(out)
-    return out
-
-    
-LEARNING_RATE = 0.004
-MOMENTUM = 0.5
-EPOCHS = 150 
-BATCH_SIZE = 128 
-IMAGE_SIZE = 224 
-NUM_WORKER = 8 
-CHECKPOINT = "./checkpoints/Tue Apr  4 22:59:33 2023"
-
-model = ResNet50(Resblock, 3, 196)
-model = model.to(device)
-print(model)
-
+# Transforms
 train_transform = transforms.Compose([
     transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.RandomHorizontalFlip(p=0.5),
@@ -124,99 +49,120 @@ test_transform = transforms.Compose([
     )
 ])
 
-optimizer = torch.optim.SGD(model.parameters(), lr = LEARNING_RATE, momentum = MOMENTUM)
+# Data
+train_data = datasets.StanfordCars(root = "./data", split = "train", transform = train_transform, download = True)
+train_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER)
+test_data = datasets.StanfordCars(root = "./data", split = "test", transform = test_transform, download = True)
+test_dataloader = torch.utils.data.DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER)
+
+sample_imgs, sample_labels = next(iter(train_dataloader))
+img_grid = torchvision.utils.make_grid(sample_imgs)
+writer.add_image("StanfordCarsImages", img_grid)
+
+# Model & optimizer ...
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = resnet50(weights = ResNet50_Weights.DEFAULT)
+model.to(device)
+optimizer = torch.optim.Adadelta(model.parameters())
 scaler = GradScaler()
 criterion = nn.CrossEntropyLoss()
+# Model Graph
+writer.add_graph(model = model, input_to_model = sample_imgs.to(device))
 
-if CHECKPOINT == "": 
-    cur_epoch = 0
-    datapoints = [[], []]
-else: 
-    print(f"Loading data from {CHECKPOINT}")
-    load = torch.load(CHECKPOINT + "/save.pt")
-    optimizer.load_state_dict(load["optimizer_state_dict"])
-    model.load_state_dict(load["model_state_dict"])
-    scaler.load_state_dict(load["scaler_state_dict"])
-    cur_epoch = load["epoch"]
-    datapoints = load["datapoints"]
+print("Training Start!")
 
-model.train()
+# Initialize
+train_loss = 0.0
+train_top1accuracy = 0.0
+train_top5accuracy = 0.0
+test_loss = 0.0
+test_top1accuracy = 0.0
+test_top5accuracy = 0.0
 
-train_data = torchvision.datasets.StanfordCars(root = "./data", split = "train", transform = train_transform, download = True)
-train_dataloader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER)
-test_data = torchvision.datasets.StanfordCars(root = "./data", split = "test", transform = test_transform, download = True)
-test_dataloader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER)
+# Iterate through epochs
+for epoch in range(1, EPOCHS + 1):
+    print(f"In epoch {epoch}/{EPOCHS}:")
+    # Monitor loss
+    train_loss = 0.0
+    train_top1accuracy = 0.0
+    train_top5accuracy = 0.0
+    test_loss = 0.0
+    test_top1accuracy = 0.0
+    test_top5accuracy = 0.0
 
-datasize = len(train_data)
-n_iterations = math.ceil(datasize / BATCH_SIZE)
+    # Train the model
+    model.train()
+    loop = tqdm(train_dataloader)
+    loop.set_description(f"Training Epoch[{epoch}/{EPOCHS}]")
+    for images, labels in loop:
+        optimizer.zero_grad()
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Forward pass
+        with autocast():
+            pred = model(images)
+            loss = criterion(pred, labels)
+        # Backward pass
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        # Calculate Loss & Accuracy
+        train_loss += loss.item() * images.shape[0]
+        _, indices = torch.max(pred, 1)
+        train_top1accuracy += torch.sum(indices == labels)
+        _, indices = torch.topk(pred, 5, 1)
+        train_top5accuracy += torch.sum(indices == torch.unsqueeze(labels, 1), (0, 1))
 
-plt.ion()
-plt.figure(1)
-plt.style.use('classic')
-plt.title("Loss curve")
-plt.xlabel("# of Epochs")
-plt.ylabel("Loss")
-plt.plot(datapoints[0], datapoints[1])
-plt.plot(datapoints[0], datapoints[1])
-plt.draw()
-plt.pause(0.01)
 
-for epoch in range(cur_epoch, EPOCHS):
-  avg_loss = 0
-  for i, [images, labels] in enumerate(train_dataloader):
-    optimizer.zero_grad()
-    images = images.to(device)
-    labels = labels.to(device)
-    with autocast():
-      pred = model(images)
-      loss = criterion(pred, labels)
-
-    scaler.scale(loss).backward()
-    scaler.step(optimizer)
-    scaler.update()
-    avg_loss += loss.item() * images.shape[0]
-    if (i + 1) % 10 == 0 or (i + 1) == n_iterations:
-      print(f"Loss: {loss:.7f} [iteration {i + 1}/{n_iterations} in epoch {epoch + 1}/{EPOCHS}]")
-  avg_loss /= datasize   
-
-  datapoints[0].append(epoch + 1)
-  datapoints[1].append(avg_loss)
-  plt.clf()
-  plt.style.use('classic')
-  plt.title("Loss curve")
-  plt.xlabel("# of Epochs")
-  plt.ylabel("Loss")
-  plt.plot(datapoints[0], datapoints[1])
-  plt.draw()
-  plt.pause(0.01)
-  if (epoch + 1) % 5 == 0 or (epoch + 1) == EPOCHS:
-    checkpoint = {
-        'epoch' : epoch + 1,
-        'model_state_dict' : model.state_dict(),
-        'optimizer_state_dict' : optimizer.state_dict(),
-        'scaler_state_dict': scaler.state_dict(),
-        'datapoints' : datapoints
-    }
-    filedir = "./checkpoints/" + time.asctime()
-    if not os.path.exists(filedir):
-        os.makedirs(filedir)
-    torch.save(checkpoint, filedir + "/save.pt")
-    plt.savefig(filedir + "/loss_curve.png")
+    # Evaluate the model
+    loop = tqdm(test_dataloader)
+    loop.set_description(f"Testing Epoch[{epoch}/{EPOCHS}]")
+    model.eval() 
+    with torch.no_grad():
+        for images, labels in loop:
+            images = images.to(device)
+            labels = labels.to(device)
+            # Forward pass
+            with autocast():
+                pred = model(images)
+                loss = criterion(pred, labels)
+            # Calculate Loss & Accuracy
+            test_loss += loss.item() * images.shape[0]
+            _, indices = torch.max(pred, 1)
+            test_top1accuracy += torch.sum(indices == labels)
+            _, indices = torch.topk(pred, 5, 1)
+            test_top5accuracy += torch.sum(indices == torch.unsqueeze(labels, 1), (0, 1))
     
-print("Training Complete!")
+    # Calculate & Save statistic
+    for name, param in model.named_parameters():
+        writer.add_histogram(tag = name + "_grad", values = param.grad, global_step = epoch)
+        writer.add_histogram(tag = name + "_data", values = param.data, global_step = epoch)
 
-model.eval()
+    train_loss /= len(train_data)
+    train_top1accuracy /= len(train_data)
+    train_top5accuracy /= len(train_data)
+    test_loss /= len(test_data)
+    test_top1accuracy /= len(test_data)
+    test_top5accuracy /= len(test_data)
+    print(f"In Training set: Loss = {train_loss:.8f}, Top1-accuracy = {train_top1accuracy * 100:.2f}%, Top5-accuracy = {train_top5accuracy * 100:.2f}%")
+    print(f"In Test set: Loss = {test_loss:.8f}, Top1-accuracy = {test_top1accuracy * 100:.2f}%, Top5-accuracy = {test_top5accuracy*100:.2f}%")
+    writer.add_scalars("Loss", tag_scalar_dict = {"Train" : train_loss, "Test" : test_loss}, global_step = epoch)
+    writer.add_scalars("Top1-Accuracy", tag_scalar_dict = {"Train" : train_top1accuracy,
+                                                        "Test" : test_top1accuracy}, global_step = epoch)
+    writer.add_scalars("Top5-Accuracy", tag_scalar_dict = {"Train" : train_top5accuracy,
+                                                        "Test" : test_top5accuracy}, global_step = epoch)
 
-correct = 0
-with torch.no_grad():
-  for i, [images, labels] in enumerate(test_dataloader):
-    images = images.to(device)
-    labels = labels.to(device)
-    with autocast():
-      pred = model(images)
-      loss = criterion(pred, labels)
-    _, guess = torch.max(pred, dim = 1)
-    correct += torch.sum(guess == labels)
-print(f"Accracy: {correct / len(test_data) * 100}% in {len(test_data)} tests")
+# Summary
+writer.add_hparams({
+        "epoch" : EPOCHS, 
+        "batchsize" : BATCH_SIZE,
+        "model" : "ResNet50"
+    }, {
+        "Loss" : test_loss,
+        "Top1-Accuracy" : test_top1accuracy,
+        "Top5-Accuracy" : test_top5accuracy
+    }
+)
 
-
+writer.close()
